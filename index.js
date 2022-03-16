35d20a1996c2fced43c491e5585b4c4169861a37
DevGibby <71801512+DevGibby@users.noreply.github.com>
2022-03-16 19:31:52 -0400

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-import routes from './routes';
+import routes from './routes.js';
 
 const host = process.env.HOST || '0.0.0.0.0';
 const port = process.env.PORT || 5000;
